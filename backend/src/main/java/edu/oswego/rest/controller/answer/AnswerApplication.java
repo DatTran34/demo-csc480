6a2101f1f9f9b550f162c6129992f46e081d6c02
DatTran34 <tridat1vn@gmail.com>
2021-10-14 18:24:25 -0400

    clean

--- backend/src/main/java/edu/oswego/rest/controller/answer/AnswerApplication.java
+++
@@ -1,9 +0,0 @@
-package edu.oswego.rest.controller.answer;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class AnswerApplication extends Application {
-
-}
